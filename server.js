093b339ada3e440c733c41c300234b405cb70a2b
popcicleman09 <jack@notunique.com>
2025-01-30 11:54:26 -0500

    MVC getting started and Build Inventory route steps are done

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 /* ***********************
  * View Engine and Remplates
  *************************/
@@ -23,9 +24,10 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //index route
-app.get("/", function(req,res){
-  res.render("index",{title: "Home"})
-})
+app.get("/",baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
